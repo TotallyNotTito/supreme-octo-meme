c61ebbd3274463f326e832a451d43a02558abfcf
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2021-08-12 13:56:26 -0700

    Update loop_temp.c

--- loop_temp.c
+++ loop_temp.c
@@ -15,6 +15,11 @@ main(int argc, char * argv[])
 
   while (fahr < max) {
     cels = (5 * (fahr - 32)) / 9;
+    /* The reason for multiplying by 5 and dividing by 9 instead of just multiplying by 5/9 is that in
+    C, as in many other languages, integer division truncates: any fractional part is discarded.
+    Since 5 and 9 are integers. 5/9 would be truncated to zero and so all the Celsius temperatures
+    would be reported as zero.
+    */
     printf(":\t%d\t%d\n", fahr, cels);
     fahr = fahr + step;
   } 

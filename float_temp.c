3fd55689e76bf6a4bfbfde491eaaaacf11abdd4f
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2021-08-12 14:02:02 -0700

    Add files via upload

---
+++ float_temp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+main(int argc, char * argv[])
+{
+  if (argc < 2) {
+    printf("Error\n");
+    return -1;
+  } 
+  float fahr, cels;
+  int step = 10;
+  int max = 200;
+  fahr = atof(argv[1]);
+
+  while (fahr < max) {
+    cels = (5.0/9.0) * fahr;
+    printf(":\t%f\t%f\n", fahr, cels);
+    fahr = fahr + step;
+  } 
+
+  return 0;
+}
